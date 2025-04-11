e8efe4432cf7d7803e5bb8405116a2e99cc5f72c
WailsCarton <regansasikumar29@gmail.com>
2025-04-11 10:13:18 +0200

    Home page + redirection candidat/recruteur fait

---
+++ src/app/candidat/page.js
@@ -0,0 +1,9 @@
+export default function CandidatPage() {
+    return (
+      <div style={{ padding: '2rem' }}>
+        <h1>Page Candidat</h1>
+        <p>Bienvenue sur la page dédiée aux candidats.</p>
+      </div>
+    );
+  }
+  
---
+++ src/app/recruteur/page.js
@@ -0,0 +1,9 @@
+export default function RecruteurPage() {
+    return (
+      <div style={{ padding: '2rem' }}>
+        <h1>Page Recruteur</h1>
+        <p>Bienvenue sur la page dédiée aux recruteurs.</p>
+      </div>
+    );
+  }
+  
